336afac6a98242c26e9630853773b51606bd0daf
ManasviSahu1503 <manasvi.sahu03@gmail.com>
2026-09-07 20:10:05 +0530

    Added python basic

---
+++ python/basics/variables.py
@@ -0,0 +1,27 @@
+# Variables in Python
+
+name = "Varsha"
+age = 21
+height = 5.9
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Student:", is_student)
+
+# Multiple variables
+x, y, z = 10, 20, 30
+
+print(x, y, z)
+
+# Changing a variable
+age = 22
+print("Updated age:", age)
+
+# Variable naming
+first_name = "Varsha"
+college_name = "ABC College"
+
+print(first_name)
+print(college_name)
